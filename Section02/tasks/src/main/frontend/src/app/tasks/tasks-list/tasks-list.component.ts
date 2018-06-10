a7bf56c0cce3ad2320b9a0c288247173db8e9fa0
krit <krit.t@ncrthailand.com>
2018-06-11 06:07:22 +0700

    lecture 16 - working

--- Section02/tasks/src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ Section02/tasks/src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../Task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksListComponent implements OnInit {
 
+  tasks: Task[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.tasks.push(new Task(1, "Test Task 1", true, "07/08/17"));
+    this.tasks.push(new Task(2, "Test Task 2", true, "07/08/17"));
+    this.tasks.push(new Task(3, "Test Task 3", false, "07/08/17"));
+  }
+
+  getDueDateLabel(task:Task){
+    return task.completed ? 'label-success' : 'lable-primary';
+  }
+
+  onTaskChange(event, task){
+    //return this.task
   }
 
 }
