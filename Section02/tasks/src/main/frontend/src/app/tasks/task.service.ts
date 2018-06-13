628e742e5c3962ceecc081c472ecffd21b10a4f7
krit <krit.t@ncrthailand.com>
2018-06-13 23:07:22 +0700

    Lecture 18 - task save

--- Section02/tasks/src/main/frontend/src/app/tasks/task.service.ts
+++ Section02/tasks/src/main/frontend/src/app/tasks/task.service.ts
@@ -1,6 +1,7 @@
 import {Http} from '@angular/http';
 import {Injectable} from '@angular/core';
 import 'rxjs/add/operator/map';
+import { Task } from './Task.model';
 
 @Injectable()
 export class TaskService{
@@ -9,6 +10,11 @@ export class TaskService{
     }
 
     getTasks(){
-        return this.http.get('/api/tasks').map(response => response.json())
+        return this.http.get('/api/tasks').map(response => response.json());
+    }
+
+    saveTask(task:Task, checked: boolean){
+        task.completed = checked;
+        return this.http.post('/api/tasks/save', task).map(response => response.json());
     }
 }
